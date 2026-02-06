7f3395faae925c1414032b0360f19063a1e8979f
Steve Gontzes <gontzess@gmail.com>
2026-02-05 20:17:24 -0500

    refactor: use generated config type instead of viper
    Update main.go to use SDK generated config type Segment.

--- cmd/baton-segment/main.go
+++ cmd/baton-segment/main.go
@@ -7,13 +7,11 @@ import (
 
 	"github.com/conductorone/baton-sdk/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
-	"github.com/conductorone/baton-sdk/pkg/connectorrunner"
 	"github.com/conductorone/baton-sdk/pkg/types"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
-	"github.com/spf13/viper"
 	"go.uber.org/zap"
 
-	segmentconfig "github.com/conductorone/baton-segment/pkg/config"
+	cfg "github.com/conductorone/baton-segment/pkg/config"
 	"github.com/conductorone/baton-segment/pkg/connector"
 )
 
@@ -26,8 +24,7 @@ func main() {
 		ctx,
 		"baton-segment",
 		getConnector,
-		segmentconfig.ConfigurationSchema,
-		connectorrunner.WithDefaultCapabilitiesConnectorBuilder(&connector.Segment{}),
+		cfg.Config,
 	)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
@@ -43,20 +40,20 @@ func main() {
 	}
 }
 
-func getConnector(ctx context.Context, v *viper.Viper) (types.ConnectorServer, error) {
+func getConnector(ctx context.Context, c *cfg.Segment) (types.ConnectorServer, error) {
 	l := ctxzap.Extract(ctx)
 
-	cb, err := connector.New(ctx, v.GetString(segmentconfig.TokenField.FieldName))
+	cb, err := connector.New(ctx, c.Token)
 	if err != nil {
 		l.Error("error creating connector", zap.Error(err))
 		return nil, err
 	}
 
-	c, err := connectorbuilder.NewConnector(ctx, cb)
+	conn, err := connectorbuilder.NewConnector(ctx, cb)
 	if err != nil {
 		l.Error("error creating connector", zap.Error(err))
 		return nil, err
 	}
 
-	return c, nil
+	return conn, nil
 }
